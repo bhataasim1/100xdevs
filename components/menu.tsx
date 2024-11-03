b985c8f42cf7e1d27c712373ed2f6102cfde70df
bhataasim1 <bhataasim700@gmail.com>
2024-11-03 20:23:30 +0530

    add navigation menu component for site header

---
+++ components/menu.tsx
@@ -0,0 +1,61 @@
+import Link from 'next/link'
+import * as React from 'react'
+
+import {
+   NavigationMenu,
+   NavigationMenuItem,
+   NavigationMenuLink,
+   NavigationMenuList,
+   navigationMenuTriggerStyle,
+} from 'components/ui/navigation-menu'
+import { siteConfig } from 'lib/config'
+import { cn } from 'lib/utils'
+
+export default function NavigationMenuDemo() {
+   return (
+      <NavigationMenu>
+         <NavigationMenuList>
+            {siteConfig.header.map((item, index) => (
+               <NavigationMenuItem key={index}>
+                  <Link
+                     href={item.href || ''}
+                     legacyBehavior
+                     passHref
+                  >
+                     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        {item.label}
+                     </NavigationMenuLink>
+                  </Link>
+               </NavigationMenuItem>
+            ))}
+         </NavigationMenuList>
+      </NavigationMenu>
+   )
+}
+
+const ListItem = React.forwardRef<
+   React.ElementRef<'a'>,
+   React.ComponentPropsWithoutRef<'a'>
+>(({ className, title, children, ...props }, ref) => {
+   return (
+      <li>
+         <NavigationMenuLink asChild>
+            <a
+               ref={ref}
+               className={cn(
+                  'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+                  className,
+               )}
+               {...props}
+            >
+               <div className="text-sm font-medium leading-none">{title}</div>
+               <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                  {children}
+               </p>
+            </a>
+         </NavigationMenuLink>
+      </li>
+   )
+})
+
+ListItem.displayName = 'ListItem'
